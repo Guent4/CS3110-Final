c66bff805c1faae7ddc277a82620f25a115f1718
hg299 <hg299@cornell.edu>
2015-11-22 21:46:23 -0500

    renamed and deleted unused files

--- lexer.mli
+++
@@ -1,6 +0,0 @@
-open Lexing
-
-(* This is responsible for taking the string input from the reader and lexing
- * it into a list of strings that will later be parsing into commands
- *)
-val lex : string -> string list
