175c152c83e209337646e9c36c31a2c564adaa79
Guent4 <davidgu96@gmail.com>
2015-12-02 00:40:45 -0500

    merged in David

--- src/camel/atlas.ml
+++ src/camel/atlas.ml
@@ -1,12 +1,28 @@
 open Coconuts
 
+(* This ml is responsible for providing the constants that will be used in the
+ * camel modules.  Along with some of the constants, some general helper
+ * functions related to the constants are also included. *)
 
+
+
+
+(* Location of the file that contains documentation on the commands relative to
+ * the home directory *)
 let doc_loc = "./OASys_Doc.txt"
+(* Location of the file that contains the help articles relative to the home
+ * directory  *)
 let help_loc = "./OASys_Help.txt"
 
+(* List of all of the accepted commands *)
 let cmd_str_list = ["init";"log";"status";"add";"commit";"branch";"checkout";
   "reset";"rm";"diff";"merge";"config";"push";"pull";"clone";"help";"quit"]
 
+(* [translate_cmd cmd_string] - Converts string form of a command to the cmd form.
+ *      If string does not correspond to a cmd, then a INVALID_CMD is returned.
+ * Parameters:
+ *    - cmd_string - the string that is to be converted to a cmd
+ * Return: returns the cmd of the string; INVALID_CMD if cmd_string has no conversion *)
 let translate_cmd (cmd_string:string) : cmd =
   match cmd_string with
   | "init"          -> INIT
@@ -28,6 +44,10 @@ let translate_cmd (cmd_string:string) : cmd =
   | "quit"          -> ignore(exit 0); QUIT
   | s               -> INVALID_CMD s
 
+(* [detranslate_cmd cmd] - Converts a cmd into the string version of it
+ * Parameters:
+ *    - cmd - the cmd that is to be converted to a string
+ * Return: the string version of the cmd *)
 let detranslate_cmd (cmd:cmd) : string =
   match cmd with
   | INIT            -> "init"
@@ -49,9 +69,15 @@ let detranslate_cmd (cmd:cmd) : string =
   | QUIT            -> "quit"
   | INVALID_CMD s   -> s
 
+(* List of all of the accepted opt inputs *)
 let opt_str_list = ["--message";"--all";"--set-upstream";"--delete";"--remove";
   "--rename";"--branch";"--file";"--hard";"--mixed";"--soft";"--add"]
 
+(* [translate_opt opt_string] - Converts string form of a opt to the opt form.
+ *      If string does not correspond to a opt, then a INVALID_OPT is returned.
+ * Parameters:
+ *    - opt_string - the string that is to be converted to a opt
+ * Return: returns the opt of the string; INVALID_OPT if opt_string has no conversion *)
 let translate_opt (opt_string:string) : opt =
   match opt_string with
   | "-m" | "--message"          -> MSG
@@ -70,6 +96,10 @@ let translate_opt (opt_string:string) : opt =
   | ""                          -> EMPTY
   | s                           -> INVALID_OPT s
 
+(* [detranslate_opt opt] - Converts a opt into the string version of it
+ * Parameters:
+ *    - opt - the opt that is to be converted to a string
+ * Return: the string version of the opt *)
 let detranslate_opt (opt:opt) : string =
   match opt with
   | MSG             -> "-m or --message"
@@ -88,7 +118,15 @@ let detranslate_opt (opt:opt) : string =
   | EMPTY           -> "<no options given>"
   | INVALID_OPT s   -> "s"
 
+(* A cmd * opt map that is used to create the mpa of the expected number of args
+ * and the default opt *)
 module M = Map.Make (struct type t = (cmd * opt) let compare a b = Pervasives.compare a b end)
+
+(* Map used to determine how many args a cmd * opt pair should have.  All other
+ * combinations of cmd * opt are not supported by OASys.
+ * Key - A cmd * opt pair that is accepted by OASys
+ * Value - A list corresponding to the number of args that the cmd * opt pair
+ *      can support.  -1 corresponds to > 0 and -2 corresponds to >= 0 *)
 let arg_num_expected =
   let lst =
     [ ((INIT,EMPTY),[0]);
@@ -109,6 +147,10 @@ let arg_num_expected =
     ] in
   List.fold_left (fun acc x -> match x with | (x,y) -> M.add x y acc) M.empty lst
 
+(* Map used to determine what the default opt is if no opt is given for the cmd
+ * Key - cmd * EMPTY (Note QUIT and INVALID_CMD are not supported because they
+ *      cannot have a default)
+ * Value - The default opt *)
 let arg_num_default =
   let lst =
     [ ((INIT,EMPTY),EMPTY);
@@ -130,7 +172,13 @@ let arg_num_default =
     ] in
   List.fold_left (fun acc x -> match x with | (x,y) -> M.add x y acc) M.empty lst
 
-  let print_error ?s1:(s1="") ?s2:(s2="") ?i1:(i1=0) ?i2:(i2=0) ?i3:(i3=0) ?i4:(i4=0) = function
+(* [print_error ?s1 ?s2 ?i1 ?i2 n] - print the error corresponding to n
+ * Parameters:
+ *    - n - The int corresponding to the desired error message
+ *    - s1,s2 - Optional strings that are to be included in the error
+ *    - i1,i2 - Optional ints that are to be included in the error
+ * Return: unit (the error is directly printed) *)
+let print_error ?s1:(s1="") ?s2:(s2="") ?i1:(i1=0) ?i2:(i2=0) = function
   | 0 -> Printf.printf "\nWelcome to OASys!\nCreated by Gu, Ho, Moheed, and Ramalingam\n\nFor help, refer to the readme.txt file in the folder\nTo search up info about a specific command, type \"help --cmd command_name\".\nTo search up more general topics, type \"help search_word(s)\".\n\nFor more questions, contact ____\n\n"
   | 1 -> Printf.printf "FAILURE: \"%s\" is an invalid option.\n" s1
   | 2 -> Printf.printf "FAILURE: The \"%s\" command does not support more than 1 option.\n" s1
