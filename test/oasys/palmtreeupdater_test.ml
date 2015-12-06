0b43e9f0f7849ab0beba467511aa90451cd9ebc3
fmoheed <fmoheed@gmail.com>
2015-12-05 20:28:07 -0500

    finished init, add, add all, remove, and commit tests

--- test/oasys/palmtreeupdater_test.ml
+++ test/oasys/palmtreeupdater_test.ml
@@ -1,5 +1,6 @@
 open Coconuts
 open Palmtreeupdater
+open Core.Std
 
 let setup_tree () =
   let () = Fileio.remove_dir "./test_proj/.oasys/" in
@@ -13,16 +14,26 @@ let init tree config =
   let (tree',config',feedback) = update_tree cmd tree config in
   (tree',config',feedback)
 
-(* let add tree config filename =
-  let cmd = (ADD,[],[filename]) in
+let add tree config filename =
+  let cmd = (ADD,[EMPTY],[filename]) in
   let (tree',config',feedback) = update_tree cmd tree config in
-  (tree',config',feedback) *)
+  (tree',config',feedback)
 
-(* let commit tree config msg =
-  let cmd = (COMMIT,[],[msg]) in
+let add_all tree config = 
+  let cmd = (ADD,[ALL],[]) in 
   let (tree',config',feedback) = update_tree cmd tree config in
-  (tree',config',feedback) *)
+  (tree',config',feedback)
 
+let remove tree config filename = 
+  let cmd = (RM,[FILE],[filename]) in
+  let (tree',config',feedback) = update_tree cmd tree config in
+  (tree',config',feedback)
+
+let commit tree config msg =
+  let cmd = (COMMIT,[MSG],[msg]) in
+  let (tree',config',feedback) = update_tree cmd tree config in
+  (tree',config',feedback)
+(*****************************************************************************)
 TEST_MODULE "init tests" = struct
   let (tree,config) = setup_tree ()
   let (tree',config',feedback) = init tree config
@@ -72,71 +83,121 @@ TEST_MODULE "init tests" = struct
     config'' = config'
   )
 end
+(*****************************************************************************)
+TEST_MODULE "add, add_all and remove tests" = struct
+  (*TEST FILES*)
+  let () = Out_channel.write_all "./test_proj/wuggle.txt" ~data:"david"
+  let () = Out_channel.write_all "./test_proj/test.txt" ~data:"david"
 
-(* TEST_MODULE "add tests" = struct
-  let (tree,config) = setup_tree ()
-  let (tree,config,feedback) = init tree config
-  let (tree',config',feedback) = add tree config "wuggle.txt"
-  TEST_UNIT "test tree" = assert
+  (*ADD TESTS*)
+  (*Create file to add*)
+  let (tree,config) = setup_tree () 
+  let (tree,config,feedback) = init tree config 
+  let (tree',config',feedback) = add tree config "wuggle.txt" 
+  TEST_UNIT "test added file" = assert
   (
-    let master = CommitTree.find "master" tree' in
-    match master with
-    | Changes(["wuggle.txt"],[],[]) :: Commit (_,"initial commit") :: [] -> true
-    | _ -> false
+    let (ad, rem) = tree'.index in
+    List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle.txt")
   )
   TEST_UNIT "test config" = assert
   (
     config' = config
   )
-  TEST_UNIT "test feedback" = assert
+  
+  (*Create another file to add*)
+  (* let () = Out_channel.write_all "./test_proj/test.txt" ~data:"david" *)
+  let (tree'',config'',feedback) = add tree' config' "test.txt"
+  TEST_UNIT "test added file" = assert
   (
-      match feedback with
-      | Success "wuggle.txt has been added" -> true
-      | _ -> false
+    let (ad, rem) = tree''.index in
+    List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "test.txt")
   )
-  let (tree'',config'',feedback) = add tree' config' "wuggle.txt"
-  TEST_UNIT "test tree (x2)" = assert
-  (
-    let master = CommitTree.find "master" tree'' in
-    match master with
-    | Changes(["wuggle.txt"],[],[]) :: Commit (_,"initial commit") :: [] -> true
-    | _ -> false
-  )
+
   TEST_UNIT "test config (x2)" = assert
   (
     config'' = config'
   )
-  TEST_UNIT "test feedback (x2)" = assert
-  (
-      match feedback with
-      | Success "wuggle.txt has been added" -> true
-      | Success x | Failure x ->
-      (let () = Printf.printf "\n%s\n" x in
-      false)
-  )
-end
 
+  (*REMOVE TESTS*)
+  (*Remove wuggle.txt*)
+  let (rmtree, rmconfig, rmfeedback) = remove tree'' config'' "wuggle.txt"
+  TEST_UNIT "remove wuggle.txt" = assert 
+  (
+    let (ad, rem) = rmtree.index in
+    let wug = List.mem rem ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle.txt") in
+    let wug2 = List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "test.txt") in
+    wug && wug2
+  )
+  TEST_UNIT "test rm config" = assert
+  (
+    rmconfig = config'
+  )
+  (*Remove test.txt*)
+  let (rmtree', rmconfig', rmfeedback) = remove rmtree rmconfig "test.txt"
+  TEST_UNIT "Remove test.txt" = assert
+  (
+    let (ad, rem) = rmtree'.index in
+    let wug = List.mem rem ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle.txt") in
+    let wug2 = List.mem rem ((Sys.getcwd()) ^ "/test_proj/"  ^ "test.txt") in
+    wug && wug2
+  )
+  TEST_UNIT "test rm config (x2)" = assert
+  (
+    rmconfig' = rmconfig
+  )
+ 
+  (* (*ADD ALL TESTS*)
+  let (alltree, allconfig, allfeedback) = add_all rmtree' rmconfig'
+  TEST_UNIT = assert
+  (
+    let (ad, rem) = alltree.index in 
+    let wug = List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle.txt") in
+    let wug2 = List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle2.txt") in
+    wug && wug2
+  ) *)
+end
+(*****************************************************************************)
 TEST_MODULE "commit tests" = struct
+  (*TEST FILES*)
+  let () = Out_channel.write_all "./test_proj/wuggle.txt" ~data:"david"
+  let () = Out_channel.write_all "./test_proj/test.txt" ~data:"david"
+
   let (tree,config) = setup_tree ()
   let (tree,config,feedback) = init tree config
   let (tree',config',feedback) = add tree config "wuggle.txt"
-  TEST_UNIT "test tree" = assert
+  let (tree'', config'', feedback) = add tree' config' "test.txt"
+  let (tree3, config3, feedback) = commit tree'' config'' "test"
+  let (comtree, comconfig, comfeedback) = commit tree3 config3 "test1"
+  TEST_UNIT "test tree when files added" = assert
   (
-  let master = CommitTree.find "master" tree' in
-  match master with
-  | Changes(["wuggle.txt"],[],[]) :: Commit (_,"initial commit") :: [] -> true
-  | _ -> false
+    let (ad, rem) = tree''.index in
+    let wug = List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle.txt") in
+    let wug2 = List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "test.txt") in
+    wug && wug2
   )
-end *)
-
-(* TEST_MODULE "rm tests" = struct
-  let (tree,config) = setup_tree ()
-  let (tree',config',feedback) = init tree config
-  TEST_UNIT "rm EMPTY" =
-    let cmd = (RM,[EMPTY],[]) in
-
-
+  TEST_UNIT "test tree when files committed" = assert
+  (
+    let (ad, rem) = comtree.index in
+    let wug = List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle.txt") in
+    let wug2 = List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "test.txt") in
+    ((wug && wug2) = false)
+  )
+  TEST_UNIT "commit head test" = assert
+  (
+    let (i, m, c) = comtree.head in
+    let wug = List.mem c ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle.txt") in
+    let wug2 = List.mem c ((Sys.getcwd()) ^ "/test_proj/"  ^ "test.txt") in
+    wug && wug2 
+  )
+  TEST_UNIT "commit-tree test" = assert
+  (
+    let master = CommitTree.find "master" comtree.commit_tree in
+    match master with
+    | (i, m, c)::t -> print_endline m;(m = "test") && (List.mem c ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle.txt")) && (List.mem c ((Sys.getcwd()) ^ "/test_proj/"  ^ "test.txt"))
+    | _ -> false
+  )
 end
 
- *)
+
+
 let () = Pa_ounit_lib.Runtime.summarize ()
