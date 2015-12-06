0769861b19f456dcee94b4ac320969cb1baa3678
Rohoe <rh535@cornell.edu>
2015-12-05 23:39:39 -0500

    fixed add all test

--- test/oasys/palmtreeupdater_test.ml
+++ test/oasys/palmtreeupdater_test.ml
@@ -5,9 +5,9 @@ open Core.Std
 let setup_tree () =
 	let () = Fileio.remove_dir "./test_proj/.oasys/" in
 	let dir = (Sys.getcwd()) ^ "/test_proj/" in
-	let config = {repo_dir=dir; current_branch="master"; username=""; 
+	let config = {repo_dir=dir; current_branch="master"; username="";
 		password=""; upstream=""} in
-	let tree = {head=("", "", []); index=([],[]); work_dir=[]; 
+	let tree = {head=("", "", []); index=([],[]); work_dir=[];
 		commit_tree= CommitTree.add "master" [] (CommitTree.empty)} in
 	(tree,config)
 
@@ -56,7 +56,7 @@ TEST_MODULE "init tests" = struct
 		config = config'
 	)
 	TEST "test feedback" =(
-		let x = "Initialized empty OASYS repository in " ^ 
+		let x = "Initialized empty OASYS repository in " ^
 			config'.repo_dir ^ ".oasys/" in
 		match feedback with
 		| Success y when y = x -> true
@@ -149,15 +149,17 @@ TEST_MODULE "add, add_all and remove tests" = struct
 		rmconfig' = rmconfig
 	)
 
+	let () = Out_channel.write_all "./test_proj/wuggle.txt" ~data:"david"
+	let () = Out_channel.write_all "./test_proj/test.txt" ~data:"david"
 	(*ADD ALL TESTS*)
-(*   let (alltree, allconfig, allfeedback) = add_all rmtree' rmconfig'
+  let (alltree, allconfig, allfeedback) = add_all rmtree' rmconfig'
 	TEST_UNIT = assert
 	(
 		let (ad, rem) = alltree.index in
 		let wug = List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle.txt") in
 		let wug2 = List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "test.txt") in
 		wug && wug2
-	) *)
+	)
 end
 (*****************************************************************************)
 TEST_MODULE "commit tests" = struct
@@ -199,7 +201,7 @@ TEST_MODULE "commit tests" = struct
 	(
 		let master = CommitTree.find "master" comtree.commit_tree in
 		match master with
-		| (i, m, c)::t -> (m = "test") && 
+		| (i, m, c)::t -> (m = "test") &&
 			(List.mem c ((Sys.getcwd()) ^ "/test_proj/"  ^ "wuggle.txt")) && (List.mem c ((Sys.getcwd()) ^ "/test_proj/"  ^ "test.txt"))
 		| _ -> false
 	)
@@ -209,12 +211,12 @@ TEST_MODULE "reset FILES tests" = struct
 	let (tree,config) = setup_tree ()
 	let (tree',config',feedback) = init tree config
 	let () = Out_channel.write_all "./test_proj/add1.txt" ~data:"Your text"
-	let (tree'',config'',feedback') = update_tree (ADD,[EMPTY],["add1.txt"]) 
+	let (tree'',config'',feedback') = update_tree (ADD,[EMPTY],["add1.txt"])
 		tree' config'
 	let (ad, rem) = tree''.index
 	TEST_UNIT =
 		assert(List.mem ad ((Sys.getcwd()) ^ "/test_proj/"  ^ "add1.txt"))
-	let (tree''',config''',feedback'') = update_tree (RESET,[FILE],["add1.txt"]) 
+	let (tree''',config''',feedback'') = update_tree (RESET,[FILE],["add1.txt"])
 		tree'' config''
 	let (ad, rem) = tree'''.index
 		TEST_UNIT =
@@ -227,11 +229,11 @@ TEST_MODULE "reset tests" = struct
 	let (id,msg,committed) = tree'.head
 	(* create some files and add them and commit*)
 	let () = Out_channel.write_all "./test_proj/add1.txt" ~data:"Your text"
-	let (tree'',config'',feedback') = update_tree (ADD,[EMPTY],["add1.txt"]) 
+	let (tree'',config'',feedback') = update_tree (ADD,[EMPTY],["add1.txt"])
 		tree' config'
-	let (tree''',config''',feedback'') = update_tree (COMMIT,[MSG],["add1.txt"]) 
+	let (tree''',config''',feedback'') = update_tree (COMMIT,[MSG],["add1.txt"])
 		tree'' config''
-	let (tree4, config4, feedback3) = update_tree (RESET,[MIXED],[id]) tree''' 
+	let (tree4, config4, feedback3) = update_tree (RESET,[MIXED],[id]) tree'''
 		config'''
 	TEST_UNIT "MIXED tests" =
 		(assert (tree4.head = tree'.head));
@@ -242,7 +244,7 @@ TEST_MODULE "reset tests" = struct
 		| Success _ -> true
 		| Failure _ -> false
 		)
-	let (tree5,config5, feedback4) = update_tree (RESET,[SOFT],[id]) tree''' 
+	let (tree5,config5, feedback4) = update_tree (RESET,[SOFT],[id]) tree'''
 		config'''
 	TEST_UNIT "SOFT tests" =
 		(assert (tree5.head = tree'.head));
@@ -253,7 +255,7 @@ TEST_MODULE "reset tests" = struct
 		| Success _ -> true
 		| Failure _ -> false
 		)
-	let (tree6,config6, feedback5) = update_tree (RESET,[HARD],[id]) tree''' 
+	let (tree6,config6, feedback5) = update_tree (RESET,[HARD],[id]) tree'''
 		config'''
 	TEST_UNIT "HARD tests" =
 		(assert (tree6.head = tree'.head));
@@ -273,13 +275,13 @@ TEST_MODULE "checkout file" = struct
 	let (tree,config) = setup_tree ()
 	let (tree',config',feedback) = init tree config
 	let () = Out_channel.write_all "./test_proj/add1.txt" ~data:"Your text"
-	let (tree'',config'',feedback') = update_tree (ADD,[EMPTY],["add1.txt"]) tree' 
+	let (tree'',config'',feedback') = update_tree (ADD,[EMPTY],["add1.txt"]) tree'
 		config'
-	let (tree''',config''',feedback'') = update_tree (COMMIT,[MSG],["add1.txt"]) 
+	let (tree''',config''',feedback'') = update_tree (COMMIT,[MSG],["add1.txt"])
 		tree'' config''
 	let () = Out_channel.write_all "./test_proj/add1.txt" ~data:"Your text123"
 	TEST_UNIT "checkout file" =
-		let (_,_,f) = update_tree (CHECKOUT,[FILE],["add1.txt"]) tree''' config''' 
+		let (_,_,f) = update_tree (CHECKOUT,[FILE],["add1.txt"]) tree''' config'''
 	in
 		assert(match f with |Success _ -> true
 												|Failure _ -> false);
@@ -294,11 +296,11 @@ TEST_MODULE "branch + checkout tests" = struct
 	let (id,msg,committed) = tree'.head
 	(* create some files and add them and commit*)
 	let () = Out_channel.write_all "./test_proj/add1.txt" ~data:"Your text"
-	let (tree'',config'',feedback') = update_tree (ADD,[EMPTY],["add1.txt"]) 
+	let (tree'',config'',feedback') = update_tree (ADD,[EMPTY],["add1.txt"])
 		tree' config'
-	let (tree''',config''',feedback'') = update_tree (COMMIT,[MSG],["add1.txt"]) 
+	let (tree''',config''',feedback'') = update_tree (COMMIT,[MSG],["add1.txt"])
 		tree'' config''
-	let (tree4, config4, feedback3) = update_tree (BRANCH,[EMPTY],["hello"]) 
+	let (tree4, config4, feedback3) = update_tree (BRANCH,[EMPTY],["hello"])
 		tree''' config'''
 	TEST_UNIT "new branch" =
 		let master = CommitTree.find "master" tree4.commit_tree in
@@ -311,11 +313,11 @@ TEST_MODULE "branch + checkout tests" = struct
 		| Failure _ -> false
 		)
 	let () = Out_channel.write_all "./test_proj/add2.txt" ~data:"Your text2"
-	let (tree5,config5,feedback4) = update_tree (ADD,[EMPTY],["add2.txt"]) 
+	let (tree5,config5,feedback4) = update_tree (ADD,[EMPTY],["add2.txt"])
 		tree4 config4
-	let (tree6,config6,feedback5) = update_tree (COMMIT,[MSG],["add2.txt"]) 
+	let (tree6,config6,feedback5) = update_tree (COMMIT,[MSG],["add2.txt"])
 		tree5 config5
-	let (tree7,config7,feedback6) = update_tree (CHECKOUT,[EMPTY],["hello"]) 
+	let (tree7,config7,feedback6) = update_tree (CHECKOUT,[EMPTY],["hello"])
 		tree6 config6
 	TEST_UNIT "checkout" =
 		assert(tree7.head=tree4.head);
@@ -332,11 +334,11 @@ end
 TEST_MODULE "CONFIG" = struct
 	let (tree,config) = setup_tree ()
 	let (tree',config',feedback) = init tree config
-	let (tree2,config2,feedback1) = 
+	let (tree2,config2,feedback1) =
 		update_tree (CONFIG,[CONFIG_SET],["username";"rho"]) tree' config'
-	let (tree3,config3,feedback2) = 
+	let (tree3,config3,feedback2) =
 		update_tree (CONFIG,[CONFIG_SET],["password";"rho"]) tree2 config2
-	let (tree4,config4,feedback3) = 
+	let (tree4,config4,feedback3) =
 		update_tree (CONFIG,[CONFIG_SET],["upstream";"vagrant@127.0.0.1"]) tree3 config3
 	TEST_UNIT "username" =
 		assert(config2.username = "rho")
@@ -353,14 +355,14 @@ TEST_MODULE "PUSH AND PULL" = struct
 	let (id,msg,committed) = tree'.head
 	(* create some files and add them and commit*)
 	let () = Out_channel.write_all "./test_proj/add1.txt" ~data:"Your text"
-	let (tree'',config'',feedback') = update_tree (ADD,[EMPTY],["add1.txt"]) 
+	let (tree'',config'',feedback') = update_tree (ADD,[EMPTY],["add1.txt"])
 		tree' config'
-	let (tree''',config''',feedback'') = update_tree (COMMIT,[MSG],["add1.txt"]) 
+	let (tree''',config''',feedback'') = update_tree (COMMIT,[MSG],["add1.txt"])
 		tree'' config''
-	let (tree4,config4,feedback3) = 
-		update_tree (CONFIG,[CONFIG_SET],["upstream";"vagrant@127.0.0.1"]) 
+	let (tree4,config4,feedback3) =
+		update_tree (CONFIG,[CONFIG_SET],["upstream";"vagrant@127.0.0.1"])
 			tree''' config'''
-	let (tree4',config4',feedback3) = 
+	let (tree4',config4',feedback3) =
 		update_tree (PUSH,[EMPTY],[]) tree4 config4
 	TEST_UNIT "push" =
 		assert(tree4'=tree4);
